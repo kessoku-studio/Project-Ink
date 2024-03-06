801b40afbed9623c03075e9289e6eaa2e0a9e0c0
Steven Tian <zhenyuan.tian@mail.mcgill.ca>
2024-03-06 18:39:22 -0500

    Update CharacterAnimationData.cs

--- Assets/Scripts/Animation/CharacterAnimationData.cs
+++ Assets/Scripts/Animation/CharacterAnimationData.cs
@@ -1,10 +1,20 @@
 using UnityEngine;
 
+[System.Serializable]
+public class AttackAnimationData
+{
+    public AnimationClip startClip;
+    public AnimationClip loopClip; // This can be looped multiple times based on the skill level or effect
+    public AnimationClip endClip;
+    public float animationSpeed = 1.0f; // Default speed
+}
+
 [CreateAssetMenu(fileName = "CharacterAnimationData", menuName = "AnimationData/Character")]
 public class CharacterAnimationData : ScriptableObject
 {
     public AnimationClip spawnAnimation;
     public AnimationClip dieAnimation;
-    public AnimationClip[] attackAnimations; // Assuming characters can have multiple attacks or skills
+    public AttackAnimationData[] attackAnimations; // Updated to use the new structure
 }
+
 
